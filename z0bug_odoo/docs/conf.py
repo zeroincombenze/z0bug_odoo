5196b9dc49f9e554e387440fc207c6a7ae525bfc
zeroincombenze <info@shs-av.com>
2019-10-26 11:41:02 +0200

    first commit

--- z0bug_odoo/docs/conf.py
+++ z0bug_odoo/docs/conf.py
@@ -82,7 +82,7 @@ pygments_style = None
 #     html_theme = 'alabaster'
 # else:
 #     html_theme = 'master'
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
